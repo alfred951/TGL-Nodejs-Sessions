436dc011b7e573bc758742858f7f0c2cdfbfb754
Luis Gallego <alfredogallegom@gmail.com>
2023-11-02 00:41:09 -0500

    adding a basic express server

--- src/server.js
+++ src/server.js
@@ -1,37 +1,26 @@
-const http = require('http')
-const { readFileSync } = require('fs')
+const express = require('express')
+const path = require('path')
 
-// get all files
-const homePage = readFileSync('./public/index.html')
+const app = express()
 
-const server = http.createServer((req, res) => {
-    // console.log(req.method)
-    const url = req.url
-    console.log(`Received request for url: ${url}`)
+// home page
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./public/index.html'))
+})
 
-    // home page
-    if (url === '/') {
-        res.writeHead(200, { 'content-type': 'text/html' })
-        res.write(homePage)
-        res.end()
-    }
-    // about page
-    else if (url === '/about') {
-        res.writeHead(200, { 'content-type': 'text/html' })
-        res.write('<h1>about page</h1>')
-        res.end()
-    }
-    // 404
-    else {
-        res.writeHead(404, { 'content-type': 'text/html' })
-        res.write('<h1>page not found</h1>')
-        res.end()
-    }
+// about page
+app.get('/about', (req, res) => {
+    res.send('<h1>About page</h1>')
+})
+
+// 404
+app.get('/*', (req, res) => {
+    res.send('<h1>Page not found!</h1>')
 })
 
 const port = 3000
 const hostname = '127.0.0.1'
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 }); 
